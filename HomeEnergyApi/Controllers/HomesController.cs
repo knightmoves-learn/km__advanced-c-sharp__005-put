9436af97dcdefeb3caa250f870969d40ad0b22bb
Isaac <isaac@knightmoves.org>
2025-01-16 17:03:22 -0600

    QA

--- HomeEnergyApi/Controllers/HomesController.cs
+++ HomeEnergyApi/Controllers/HomesController.cs
@@ -40,7 +40,7 @@ namespace HomeEnergyUsageApi.Controllers
         {
             for (int i = 0; i < homesList.Count; i++)
             {
-                if(homesList[i].ownerLastName.Equals(ownerLastName))
+                if(homesList[i].ownerLastName == ownerLastName)
                 {
                     return homesList[i];
                 }
